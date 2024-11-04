ea257b921840ad4b9736cce0e4d3ece9a9b42041
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-11-04 16:42:13 +0000

    Add recruitment banner support
    - Add required component (intervention) to JS dependencies
    - Add partial
    - Add empty config file (see below)
    - Include file in lib
    - Helper to find recruitment banner for a given path
    - logic to read banners from gem config at runtime. Note that we
      don't have much guard code here - it's assumed that the config
      will be valid because it's tested during CI, so can't be
      released with invalid config.
    - Fixtures to test helpers and logic, plus tests that confirm
      that the actual config file is safely parseable

--- lib/govuk_web_banners.rb
+++ lib/govuk_web_banners.rb
@@ -1,5 +1,6 @@
 require "govuk_web_banners/application_helper"
 require "govuk_web_banners/engine"
+require "govuk_web_banners/recruitment_banner"
 require "govuk_web_banners/version"
 
 module GovukWebBanners
--- lib/govuk_web_banners/application_helper.rb
+++ lib/govuk_web_banners/application_helper.rb
@@ -1,4 +1,7 @@
 module GovukWebBanners
   module ApplicationHelper
+    def recruitment_banner
+      RecruitmentBanner.for_path(request.path)
+    end
   end
 end
