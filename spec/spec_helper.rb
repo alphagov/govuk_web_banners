447526ffec20307aa43bff35c63caf71719ed630
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-11-14 10:22:46 +0000

    configure spec helper
    - We'll be a bit aggressive and demand 100% test coverage,
      given we want the gem to be trusted by the calling apps.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require "simplecov"
 SimpleCov.start "rails" do
   enable_coverage :branch
-  minimum_coverage 95
+  minimum_coverage 100
   add_filter "/lib/govuk_web_banners/version.rb"
 end
 
@@ -16,4 +16,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
 GovukTest.configure
 
-RSpec.configure(&:infer_spec_type_from_file_location!)
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+  config.include ActiveSupport::Testing::TimeHelpers
+end
