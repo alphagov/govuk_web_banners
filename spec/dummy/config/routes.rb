f6f7b3e04c99bae11d66e050b51909f6f27356df
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-11-28 15:40:28 +0000

    Add dummy app routes for emergency banner tests
    - The banner is intended to be called as a parameter for
      the layout_for_public component, so we call it that way
      in the dummy app. This means we need to include a bunch
      of dependencies for that component (it wants an application
      js/css file, and updates to the manifest)
    - We also add the initializer that sets the redis client to be
      used.
    - Add a / (equivalent to the homepage) to check the homepage
      local variable causes the homepage variant to be rendered.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   get "/recruitment-with-banners", to: "banner_pages#recruitment"
   get "/recruitment-with-no-banners", to: "banner_pages#recruitment"
+
+  get "/emergency", to: "banner_pages#emergency"
+  get "/", to: "banner_pages#emergency_on_homepage"
 end
