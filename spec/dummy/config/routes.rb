4e38a60f86e5affd17b14c97822fce2396ae8dfd
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-11-28 15:39:52 +0000

    Prepare existing tests for emergency banners
    - So we don't have to spec out redis for the existing tests, we rename the dummy app routes to something relevant to the banners they're testing.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   mount GovukPublishingComponents::Engine, at: "/component-guide"
   mount GovukWebBanners::Engine => "/govuk_web_banners"
 
-  get "/page-with-banners", to: "banner_pages#show"
-  get "/page-with-no-banners", to: "banner_pages#show"
+  get "/recruitment-with-banners", to: "banner_pages#recruitment"
+  get "/recruitment-with-no-banners", to: "banner_pages#recruitment"
 end
