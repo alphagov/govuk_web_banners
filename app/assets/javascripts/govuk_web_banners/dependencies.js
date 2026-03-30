f244250db43bf1a7eaf8ea30f55be0c06b7a7a42
Sonia Hussain <56222256+CodeSonia@users.noreply.github.com>
2026-03-30 22:29:44 +0100

    Add UpratingBanner view partial
    Creates a new partial `_uprating_banner.html.erb` that displays the temporary UpratingBanner on Smart Answer pages. The banner uses the GOV.UK Publishing Components notice component and shows the banner text as the title.

--- app/assets/javascripts/govuk_web_banners/dependencies.js
+++ app/assets/javascripts/govuk_web_banners/dependencies.js
@@ -1,2 +1,3 @@
 //= require govuk_publishing_components/components/intervention
 //= require govuk_publishing_components/components/global-banner
+//= require govuk_publishing_components/components/notice
